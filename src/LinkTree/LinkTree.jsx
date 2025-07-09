5af393776c1dae19ed48b1b9ec95d93c74128c3a
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2025-07-09 19:08:07 -0300

    Adicionado links no link tree

--- src/LinkTree/LinkTree.jsx
+++ src/LinkTree/LinkTree.jsx
@@ -1,7 +1,12 @@
 import "./LinkTree.css";
 
 export default function LinkTree() {
-  const links = [{ name: "Live Pix", url: "https://livepix.gg/iacarvalho" }];
+  const links = [
+    { name: "Live Pix", url: "https://livepix.gg/iacarvalho" },
+    { name: "Discord", url: "https://discord.gg/2pqzUqWt" },
+    { name: "BlueSky", url: "https://bsky.app/profile/iacarvalho.bsky.social" },
+    { name: "Twitch", url: "https://www.twitch.tv/afonsoverse" },
+  ];
 
   return (
     <div className="link-tree">
